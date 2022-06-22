c2b248a84d4b7de75794110aadfb8704a4e6ec81
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-06-22 18:19:22 +0200

    creato array e usato ciclo for per stampare a schermo risultati partite

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,1 +1,38 @@
-<?php ?>
+<?php 
+$games=[
+    [
+    'firstsquad' => 'venezia',
+    'secondsquad' => 'milano',
+    'pointsFirstSquad' => '60',
+    'pointsSecondSquad' => '36'
+    ],
+    [
+    'firstsquad' => 'padova',
+    'secondsquad' => 'torino',
+    'pointsFirstSquad' => '80',
+    'pointsSecondSquad' => '54'
+    ],
+    [
+    'firstsquad' => 'roma',
+    'secondsquad' => 'genova',
+    'pointsFirstSquad' => '78',
+    'pointsSecondSquad' => '68'
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php for($i =0 ; $i < count($games); $i++) {?>
+        <h3><?php echo $games[$i]['firstsquad'].'-'.$games[$i]['secondsquad'].'|'.$games[$i]['pointsFirstSquad'].'-'.$games[$i]['pointsSecondSquad'];?></h3>
+    <?php } ?>
+</body>
+</html>
